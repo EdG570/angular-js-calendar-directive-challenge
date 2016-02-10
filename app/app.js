85c4f30c81f553bf14ae0a015fb6a6573d217c80
Ed Glazenski <edglazenski@gmail.com>
2016-02-10 16:58:46 -0500

    Calendar is working on page load. Loads current month and current year. Previous and next months book end styles are applied.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,61 @@
-angular.module('calendarDemoApp', []);
+var app = angular.module('calendarDemoApp', []);
+  
+  app.directive('giveMonthsYears', function() {
+      return {
+        restrict: 'EA',
+        templateUrl: 'monthsyears.html',
+        scope: true,
+        controller: function($scope, $element, $attrs) {
+            $scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            $scope.date = new Date();
+            $scope.currentMonth = $scope.date.getMonth();
+            $scope.month = $scope.currentMonth;
 
-// your controller and directive code go here
+            $scope.year = $scope.date.getFullYear();
+
+            $scope.$watch($attrs.ngModel, function() {
+              console.log($scope.month);
+            });
+            
+        }
+      };
+  });
+
+  app.directive('showCalendar', function() {
+      return {
+        restrict: 'EA',
+        templateUrl: 'calendar.html',
+        scope: true,
+        controller: function($scope, $element, $attrs) {
+          $scope.calendarData = CalendarRange.getMonthlyRange(new Date());
+          $scope.days = $scope.calendarData.days;
+
+          $scope.date = new Date();
+          $scope.currentMonth = $scope.date.getMonth();
+          console.log($scope.days);
+
+          $scope.weekOne = [];
+          $scope.weekTwo = [];
+          $scope.weekThree = [];
+          $scope.weekFour = [];
+          $scope.weekFive = [];
+
+          buildWeeks($scope.days, $scope.weekOne, 0, 7);
+          buildWeeks($scope.days, $scope.weekTwo, 7, 14);
+          buildWeeks($scope.days, $scope.weekThree, 14, 21);
+          buildWeeks($scope.days, $scope.weekFour, 21, 28);
+          buildWeeks($scope.days, $scope.weekFive, 28, 35);
+
+          function buildWeeks(days, week, start, limit) {
+        
+            for(var i = start; i < limit; i++) {
+                week.push(days[i]); 
+            }
+             return week;
+          }
+        }
+      };
+  });
+
+  
+
