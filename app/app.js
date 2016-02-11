cc2b5f8c5adaf6a2aeb1bd740b44fb70a1a518f0
Ed Glazenski <edglazenski@gmail.com>
2016-02-10 22:54:14 -0500

    Calendar now updates when a new month or year is selected

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 var app = angular.module('calendarDemoApp', []);
+
+  // app.factory('getCurrentMonthAndYear', function() {
+  //       return {
+  //         date: function() { new Date(); },
+  //         month: function() { date.getMonth(); },
+  //         year: function() { date.getFullYear(); }
+  //       };
+  // });
   
   app.directive('giveMonthsYears', function() {
       return {
@@ -6,20 +14,16 @@ var app = angular.module('calendarDemoApp', []);
         templateUrl: 'monthsyears.html',
         scope: true,
         controller: function($scope, $element, $attrs) {
-            $scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            $scope.date = new Date();
-            $scope.currentMonth = $scope.date.getMonth();
-            $scope.month = $scope.currentMonth;
-
-            $scope.year = $scope.date.getFullYear();
-
-            $scope.$watch($attrs.ngModel, function() {
-              console.log($scope.month);
-            });
             
+
+            $scope.getDate = function() {
+              return new Date($scope.year, $scope.month, 1);
+            };
+
         }
       };
   });
+
 
   app.directive('showCalendar', function() {
       return {
@@ -27,24 +31,42 @@ var app = angular.module('calendarDemoApp', []);
         templateUrl: 'calendar.html',
         scope: true,
         controller: function($scope, $element, $attrs) {
-          $scope.calendarData = CalendarRange.getMonthlyRange(new Date());
-          $scope.days = $scope.calendarData.days;
-
+          $scope.months = [{ name: 'January', id: 0 },{ name: 'february', id: 1 }, { name: 'March', id: 2 },{ name: 'April', id: 3 }, { name: 'May', id: 4 }, { name: 'June', id: 5 }, { name: 'July', id: 6 }, {name: 'August', id: 7 }, { name: 'September', id: 8 }, { name: 'October', id: 9 }, { name: 'November', id: 10 }, { name: 'December', id: 11}];
           $scope.date = new Date();
-          $scope.currentMonth = $scope.date.getMonth();
-          console.log($scope.days);
+          $scope.month = $scope.date.getMonth();
+          $scope.year = $scope.date.getFullYear();
+          $scope.years = [];
 
-          $scope.weekOne = [];
-          $scope.weekTwo = [];
-          $scope.weekThree = [];
-          $scope.weekFour = [];
-          $scope.weekFive = [];
+          for(i = $scope.year - 20; i < $scope.year + 20; i++) {
+              $scope.years.push(i);
 
-          buildWeeks($scope.days, $scope.weekOne, 0, 7);
-          buildWeeks($scope.days, $scope.weekTwo, 7, 14);
-          buildWeeks($scope.days, $scope.weekThree, 14, 21);
-          buildWeeks($scope.days, $scope.weekFour, 21, 28);
-          buildWeeks($scope.days, $scope.weekFive, 28, 35);
+          } 
+          
+          $scope.buildCalendar = function() {
+             console.log($scope.year + ' ' + $scope.month);
+
+            $scope.date = new Date($scope.year, $scope.month, 1);
+
+            //Get current month data
+            $scope.calendarData = CalendarRange.getMonthlyRange($scope.date);
+            $scope.days = $scope.calendarData.days;
+            console.log($scope.date);
+
+            //Separates month data object into weeks and stores the values in arrays for each week
+            $scope.weekOne = [];
+            $scope.weekTwo = [];
+            $scope.weekThree = [];
+            $scope.weekFour = [];
+            $scope.weekFive = [];
+
+            buildWeeks($scope.days, $scope.weekOne, 0, 7);
+            buildWeeks($scope.days, $scope.weekTwo, 7, 14);
+            buildWeeks($scope.days, $scope.weekThree, 14, 21);
+            buildWeeks($scope.days, $scope.weekFour, 21, 28);
+            buildWeeks($scope.days, $scope.weekFive, 28, 35);
+          };
+
+          $scope.buildCalendar();
 
           function buildWeeks(days, week, start, limit) {
         
